672e89f006c644923b2f44b61b2a9d049bd64333
myiuare <slealsemedo@gmail.com>
2024-11-28 17:29:23 +0100

    Update Menu3.0.php

--- Menu3.0.php
+++ Menu3.0.php
@@ -5,48 +5,31 @@
     <title>Menu</title>
 </head>
 <body>
-
-<nav>
-    <ul>
-        <li><a href="Menu2.0.php">Menu</a></li>
-        <li><a href="me_contacter.php">Me Contacter</a></li>
-    </ul>
-</nav>
+<?php require_once(__DIR__ . '/header.php'); ?>
 
 <?php 
 
-// Connexion à MySQL avec mysqli
-$mysqli = new mysqli('localhost', 'root', '', 'eatsmartprojet');
+// on se connecte à MySQL
+$mysqlClient = new PDO('mysql:host=localhost;dbname=eatsmartprojet;charset=utf8', 'root', '');
 
-// Vérification de la connexion
-if ($mysqli->connect_error) {
-    die('Erreur de connexion (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
+// on crée la requête SQL
+$sql = 'SELECT nom,prix,description FROM article';
 
-// Définir l'encodage des caractères
-$mysqli->set_charset("utf8");
+// on envoie la requête
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 
-// Création de la requête SQL
-$sql = 'SELECT nom, prix, description FROM article';
-
-// Exécution de la requête
-if ($result = $mysqli->query($sql)) {
-    // Boucle à travers chaque enregistrement
-    while ($data = $result->fetch_assoc()) {
-        // Affichage des informations de l'enregistrement
-        echo '<b>' . htmlspecialchars($data['nom']) . ' ' . htmlspecialchars($data['prix']) . ' €</b>';
-        echo ' <i>Description : ' . htmlspecialchars($data['description']) . '</i><br>';
+// on fait une boucle qui va faire un tour pour chaque enregistrement
+while($data = mysql_fetch_assoc($req))
+    {
+    // on affiche les informations de l'enregistrement en cours
+    echo '<b>'.$data['nom'].' '.$data['prix'].'</b> ('.$data['statut'].')';
+    echo ' <i>Description : '.$data['description'].'</i><br>';
     }
 
-    // Libération du résultat
-    $result->free();
-} else {
-    // En cas d'erreur dans la requête SQL
-    echo 'Erreur SQL : ' . $mysqli->error;
-}
+// on ferme la connexion à mysql
+mysql_close();
 
-// Fermeture de la connexion
-$mysqli->close();
+<?php require_once(__DIR__ . '/footer.php'); ?>
 
 ?>
 
