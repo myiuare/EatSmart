71aea02f2bbbd05508bdd2c8b37cd4c65fb7a039
myiuare <slealsemedo@gmail.com>
2024-11-28 17:32:48 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+
+<p> Toutes les recettes sont françaises. Tout les plats sont fait maison. <p>
+</footer>
---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+
+<nav>
+    <ul>
+        <li><a href="Menu2.0.php">Menu</a></li>
+        <li><a href="me_contacter.php">Me Contacter</a></li>
+    </ul>
+</nav>
+
+</header>
