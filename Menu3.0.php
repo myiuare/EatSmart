cc34074a4532a4cefdeef2ecfa809eb94d2fdee8
myiuare <slealsemedo@gmail.com>
2024-12-04 14:52:07 +0100

    uptde 3.0

--- Menu3.0.php
+++ Menu3.0.php
@@ -9,29 +9,26 @@
 
 <?php 
 
-// on se connecte à MySQL
-$mysqlClient = new PDO('mysql:host=localhost;dbname=eatsmartprojet;charset=utf8', 'root', '');
+try {
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=eatsmartprojet;charset=utf8', 'root', '');
+    $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// on crée la requête SQL
-$sql = 'SELECT nom,prix,description FROM article';
+    $sql = 'SELECT nom, prix, description, image_url FROM article';
+    $stmt = $mysqlClient->prepare($sql);
+    $stmt->execute();
 
-// on envoie la requête
-$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-
-// on fait une boucle qui va faire un tour pour chaque enregistrement
-while($data = mysql_fetch_assoc($req))
-    {
-    // on affiche les informations de l'enregistrement en cours
-    echo '<b>'.$data['nom'].' '.$data['prix'].'</b> ('.$data['statut'].')';
-    echo ' <i>Description : '.$data['description'].'</i><br>';
+    while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo '<b>' . htmlspecialchars($data['nom']) . ' ' . htmlspecialchars($data['prix']) . '</b>';
+        echo ' <i>Description : ' . htmlspecialchars($data['description']) . '</i><br>';
+        echo '  htmlspecialchars($data['image_url']) . '</i><br>';
     }
-
-// on ferme la connexion à mysql
-mysql_close();
-
-<?php require_once(__DIR__ . '/footer.php'); ?>
+} catch (PDOException $e) {
+    echo 'Erreur de connexion : ' . $e->getMessage();
+}
 
 ?>
+
+<?php require_once(__DIR__ . '/footer.php'); ?>
 
 </body>
 </html>
